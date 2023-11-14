9857e8555eba1f5a86a4042fb190fec08d11c2a2
Jerin Joy <github@jerin.me>
2023-11-14 13:27:05 -0800

    Fixed sourcing of corporate fish config

--- homedir_files/config/fish/config.fish
+++ homedir_files/config/fish/config.fish
@@ -68,7 +68,7 @@ set -x tide_time_color 6272a4
 set -x FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
 # Place all the corporate configs here. This file is in .gitignore.
-if test -d $HOME/.config/fish/corporate.config.fish
+if test -e $HOME/.config/fish/corporate.config.fish
   source $HOME/.config/fish/corporate.config.fish
 end
 
