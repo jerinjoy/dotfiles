33b6d2f580247ecfa5e7dd3739b9dd61c6afd4ce
Jerin Joy <github@jerin.me>
2023-11-14 23:34:12 -0800

    Adding Linting and license info

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+<!--
+SPDX-FileCopyrightText: 2023 Jerin Joy
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
 # dotfiles
 
 Installs my dev environment.
--- homedir_files/config/lvim/config.lua
+++ homedir_files/config/lvim/config.lua
@@ -1,3 +1,7 @@
+-- SPDX-FileCopyrightText: 2023 Jerin Joy
+--
+-- SPDX-License-Identifier: Apache-2.0
+
 -- Read the docs: https://www.lunarvim.org/docs/configuration
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
