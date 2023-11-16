549f70e8d9b583116a2ea35479840f158d951623
Jerin Joy <github@jerin.me>
2023-11-15 21:57:33 -0800

    Look for corporate config in homedir

--- homedir_files/config/fish/config.fish
+++ homedir_files/config/fish/config.fish
@@ -6,16 +6,16 @@ set -x EDITOR nvim
 
 # These aliases depend on the binaries being available.
 function add_abbr_requiring_binaries
-  abbr -a vim nvim
-  abbr -a ls exa
-  abbr -a ltr exa -lh --git --sort=newest
-  abbr -a ltra exa -lh --git --sort=newest -a
+    abbr -a vim nvim
+    abbr -a ls exa
+    abbr -a ltr exa -lh --git --sort=newest
+    abbr -a ltra exa -lh --git --sort=newest -a
 
-  abbr -a cat "bat "
-  abbr -a kit "bat --style=plain,header "
-  abbr -a less "bat --style=plain "
-  abbr -a diff "delta"
-  abbr -a v "fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
+    abbr -a cat "bat "
+    abbr -a kit "bat --style=plain,header "
+    abbr -a less "bat --style=plain "
+    abbr -a diff delta
+    abbr -a v "fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim"
 end
 
 # Abbreviations
@@ -27,33 +27,33 @@ abbr -a gstatus "git status"
 abbr -a gsubls "git submodule status --recursive"
 
 abbr -a h "history | ag"
-abbr -a c "clear"
+abbr -a c clear
 
 switch (uname)
-  case Darwin
-    if test -d /opt/homebrew/bin
-      fish_add_path -pmP /opt/homebrew/bin
-    end
+    case Darwin
+        if test -d /opt/homebrew/bin
+            fish_add_path -pmP /opt/homebrew/bin
+        end
 
-    # Add the ccache softlinks for the compilers to the path.
-    if test -d /opt/homebrew/opt/ccache/libexec
-      fish_add_path -pmP /opt/homebrew/opt/ccache/libexec
-  end
+        # Add the ccache softlinks for the compilers to the path.
+        if test -d /opt/homebrew/opt/ccache/libexec
+            fish_add_path -pmP /opt/homebrew/opt/ccache/libexec
+        end
 
-  # These are only installed by default on macOS using Homebrew
-  add_abbr_requiring_binaries
+        # These are only installed by default on macOS using Homebrew
+        add_abbr_requiring_binaries
 end
 
 if test -d $HOME/.cargo/bin/
-  fish_add_path -pmP $HOME/.cargo/bin/
+    fish_add_path -pmP $HOME/.cargo/bin/
 end
 
 if test -d $HOME/.local/bin
-  fish_add_path -pmP $HOME/.local/bin
+    fish_add_path -pmP $HOME/.local/bin
 end
 
 if test -d $HOME/.local/bin/lvim
-  fish_add_path -aP $HOME/.local/bin/
+    fish_add_path -aP $HOME/.local/bin/
 end
 
 # Tide Colors
@@ -72,8 +72,8 @@ set -x tide_time_color 6272a4
 set -x FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
 
 # Place all the corporate configs here. This file is in .gitignore.
-if test -e $HOME/.config/fish/corporate.config.fish
-  source $HOME/.config/fish/corporate.config.fish
+if test -e $HOME/corporate.config.fish
+    source $HOME/corporate.config.fish
 end
 
 
