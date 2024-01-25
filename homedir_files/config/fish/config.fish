b6141c4478a1580ddca1641d925e157a4a0e610d
Jerin Joy <joy@rivosinc.com>
2024-01-25 12:17:32 -0800

    Only add rust to path on Darwin
    Signed-off-by: Jerin Joy <joy@rivosinc.com>

--- homedir_files/config/fish/config.fish
+++ homedir_files/config/fish/config.fish
@@ -44,12 +44,12 @@ switch (uname)
             fish_add_path -pmP /opt/homebrew/opt/ccache/libexec
         end
 
+        if test -d $HOME/.cargo/bin/
+            fish_add_path -pmP $HOME/.cargo/bin/
+        end
+
         # These are only installed by default on macOS using Homebrew
         add_abbr_requiring_binaries
-end
-
-if test -d $HOME/.cargo/bin/
-    fish_add_path -pmP $HOME/.cargo/bin/
 end
 
 if test -d $HOME/.local/bin
